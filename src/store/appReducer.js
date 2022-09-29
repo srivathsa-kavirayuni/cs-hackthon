7eac4b15076572fa21a09db60c65455cd615ccb9
Srivathsa Kavirayuni/IT/CSIN <srivathsa.kavirayuni@creditsafe.com>
2022-09-29 17:55:44 +0530

    mock data

--- src/store/appReducer.js
+++ src/store/appReducer.js
@@ -1,8 +1,8 @@
 import { mockSearchResults } from "../staticData";
 
 const initialState = {
-  // companiesList: mockSearchResults,
-  companiesList: {},
+  companiesList: mockSearchResults,
+  // companiesList: {},
 };
 
 const appReducer = (state = initialState, action) => {
@@ -13,18 +13,18 @@ const appReducer = (state = initialState, action) => {
         companiesList: action.payload,
       };
     }
-    case "GET_COMPANY": {
-      return {
-        ...state,
-        company: action.payload,
-      };
-    }
-    case "GET_RECOMMENDED": {
-      return {
-        ...state,
-        companiesList: action.payload.recommendedCompanies,
-      };
-    }
+    // case "GET_COMPANY": {
+    //   return {
+    //     ...state,
+    //     company: action.payload,
+    //   };
+    // }
+    // case "GET_RECOMMENDED": {
+    //   return {
+    //     ...state,
+    //     companiesList: action.payload.recommendedCompanies,
+    //   };
+    // }
     default:
       return state;
   }
